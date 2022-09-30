03b9aaafaf894f6f1d10499832f9225d78929d2b
Elijah Marielle Sabado <elijah.marielle.sabado@adamson.edu.ph>
2022-09-30 19:09:31 +0800

    Add files via upload

---
+++ exercise1.php
@@ -0,0 +1,23 @@
+<html>
+<head><title>Sabado_Exercise1</title></head>
+<body bgcolor = "#E2C0A5"> <font face = "Courier">
+<p></p>
+<?php echo "<h2>WDD Exercise 1 (9/30/22)</h2>";
+	$username = "Elijah Marielle S. Sabado";  
+	$program = "BS Computer Engineering";
+	$yr = "3rd year";
+	$sec = "58043";
+	$string = "Today, I will learn PHP programming.";
+	echo "I am  "."<b>".$username."</b>";
+	echo " and my course is "."<b>".$program."</b>";
+	echo "<br>";
+	echo "I am a "."<b>".$yr."</b>"." student from the WDD section "."<b>".$sec."</b>";
+	echo "<br>"."<h3>".$string."</h3>";
+
+	
+//This is the snippet for php code 
+?>
+
+
+</font></body>
+</html>
